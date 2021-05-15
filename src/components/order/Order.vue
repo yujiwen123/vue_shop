3828e37684ef9f1809e438be497d7a7f83492ca1
yujiwen <1919763608@qq.com>
2021-05-15 23:50:27 +0800

    test

---
+++ src/components/order/Order.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="">
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb></el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query">
+            <el-button slot="append" icon="el-icon-search" @click="searchOrder"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+      <!-- 订单列表数据 -->
+      <el-table :data="orderlist" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column label="订单价格" prop="order_price"></el-table-column>
+        <el-table-column label="是否付款" prop="pay_status">
+          <template slot-scope="scope">
+            <el-tag type="success" v-if="scope.row.pay_status == '1'"
+              >已付款</el-tag
+            >
+            <el-tag type="danger" v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send"></el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template slot-scope="scope">
+            {{ scope.row.create_time | dateFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+            ></el-button>
+            <el-button
+              type="success"
+              icon="el-icon-location"
+              size="mini"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import ElBreadcrumb from './childComps/ElBreadcrumb'
+
+export default {
+  name: '',
+  data() {
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10,
+      },
+      orderlist: [],
+      total: 0,
+    }
+  },
+  components: {
+    ElBreadcrumb,
+  },
+  created() {
+    this.getOrderList()
+  },
+  methods: {
+    // 获取订单列表
+    async getOrderList() {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo,
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取订单列表失败！')
+      }
+      console.log(res)
+      this.total = res.data.total
+      this.orderlist = res.data.goods
+    },
+    searchOrder() {
+      console.log(this.queryInfo)
+      this.getOrderList()
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getOrderList()
+    }
+  },
+}
+</script>
+
+<style lang='less' scoped>
+</style>
---
+++ src/components/order/childComps/ElBreadcrumb.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- 面包屑导航区 -->
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+    <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {}
+  },
+  components: {},
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+</style>
