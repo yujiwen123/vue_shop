ce04c130305a38d8341c8b10aa3081063367613c
yujiwen <1919763608@qq.com>
2021-05-16 19:08:46 +0800

    完成订单功能的开发

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -7,7 +7,11 @@
       <el-row>
         <el-col :span="8">
           <el-input placeholder="请输入内容" v-model="queryInfo.query">
-            <el-button slot="append" icon="el-icon-search" @click="searchOrder"></el-button>
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="searchOrder"
+            ></el-button>
           </el-input>
         </el-col>
       </el-row>
@@ -36,11 +40,13 @@
               type="primary"
               icon="el-icon-edit"
               size="mini"
+              @click="showBox"
             ></el-button>
             <el-button
               type="success"
               icon="el-icon-location"
               size="mini"
+              @click="showProgressBox"
             ></el-button>
           </template>
         </el-table-column>
@@ -57,11 +63,57 @@
       >
       </el-pagination>
     </el-card>
+    <!-- 修改地址的对话框 -->
+    <el-dialog
+      title="修改地址"
+      :visible.sync="addressVisible"
+      width="50%"
+      @close="addressDialogClosed"
+    >
+      <el-form
+        :model="addressForm"
+        :rules="addressFormRules"
+        ref="addressFormRef"
+        label-width="100px"
+      >
+        <el-form-item label="省市区/县" prop="address1">
+          <el-cascader
+            :options="regionData"
+            v-model="addressForm.address1"
+            :props="{ expandTrigger: 'hover' }"
+          >
+          </el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址" prop="address2">
+          <el-input v-model="addressForm.address2"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addressVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addressVisible = false"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
+    <!-- 展示物流进度的对话框 -->
+    <el-dialog title="物流进度" :visible.sync="progressVisible" width="50%">
+      <!-- 时间线 -->
+      <el-timeline>
+        <el-timeline-item
+          v-for="(activity, index) in progressInfo"
+          :key="index"
+          :timestamp="activity.time"
+        >
+          {{ activity.context }}
+        </el-timeline-item>
+      </el-timeline>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import ElBreadcrumb from './childComps/ElBreadcrumb'
+import { regionData } from 'element-china-area-data'
 
 export default {
   name: '',
@@ -74,6 +126,22 @@ export default {
       },
       orderlist: [],
       total: 0,
+      addressVisible: false,
+      addressForm: {
+        address1: [],
+        address2: '',
+      },
+      addressFormRules: {
+        address1: [
+          { required: true, message: '请选择省市区/县', trigger: 'blur' },
+        ],
+        address2: [
+          { required: true, message: '请填写详细地址', trigger: 'blur' },
+        ],
+      },
+      regionData: regionData,
+      progressVisible: false,
+      progressInfo: [],
     }
   },
   components: {
@@ -91,7 +159,7 @@ export default {
       if (res.meta.status !== 200) {
         return this.$message.error('获取订单列表失败！')
       }
-      console.log(res)
+      // console.log(res)
       this.total = res.data.total
       this.orderlist = res.data.goods
     },
@@ -106,10 +174,99 @@ export default {
     handleCurrentChange(newPage) {
       this.queryInfo.pagenum = newPage
       this.getOrderList()
-    }
+    },
+    // 展示修噶爱地址的对话框
+    showBox() {
+      this.addressVisible = true
+    },
+    addressDialogClosed() {
+      this.$refs.addressFormRef.resetFields()
+    },
+    async showProgressBox() {
+      const res = {
+        data: [
+          {
+            time: '2018-05-10 09:39:00',
+            ftime: '2018-05-10 09:39:00',
+            context: '已签收,感谢使用顺丰,期待再次为您服务',
+            location: '',
+          },
+          {
+            time: '2018-05-10 08:23:00',
+            ftime: '2018-05-10 08:23:00',
+            context:
+              '[北京市]北京海淀育新小区营业点派件员 顺丰速运 95338正在为您派件',
+            location: '',
+          },
+          {
+            time: '2018-05-10 07:32:00',
+            ftime: '2018-05-10 07:32:00',
+            context: '快件到达 [北京海淀育新小区营业点]',
+            location: '',
+          },
+          {
+            time: '2018-05-10 02:03:00',
+            ftime: '2018-05-10 02:03:00',
+            context:
+              '快件在[北京顺义集散中心]已装车,准备发往 [北京海淀育新小区营业点]',
+            location: '',
+          },
+          {
+            time: '2018-05-09 23:05:00',
+            ftime: '2018-05-09 23:05:00',
+            context: '快件到达 [北京顺义集散中心]',
+            location: '',
+          },
+          {
+            time: '2018-05-09 21:21:00',
+            ftime: '2018-05-09 21:21:00',
+            context: '快件在[北京宝胜营业点]已装车,准备发往 [北京顺义集散中心]',
+            location: '',
+          },
+          {
+            time: '2018-05-09 13:07:00',
+            ftime: '2018-05-09 13:07:00',
+            context: '顺丰速运 已收取快件',
+            location: '',
+          },
+          {
+            time: '2018-05-09 12:25:03',
+            ftime: '2018-05-09 12:25:03',
+            context: '卖家发货',
+            location: '',
+          },
+          {
+            time: '2018-05-09 12:22:24',
+            ftime: '2018-05-09 12:22:24',
+            context: '您的订单将由HLA（北京海淀区清河中街店）门店安排发货。',
+            location: '',
+          },
+          {
+            time: '2018-05-08 21:36:04',
+            ftime: '2018-05-08 21:36:04',
+            context: '商品已经下单',
+            location: '',
+          },
+        ],
+        meta: { status: 200, message: '获取物流信息成功！' },
+      }
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取物流进度失败！')
+      }
+      this.progressInfo = res.data
+      this.progressVisible = true
+      console.log(this.progressInfo)
+    },
   },
 }
 </script>
 
 <style lang='less' scoped>
+.el-cascader {
+  width: 100%;
+}
+.el-cascader-menu {
+  width: 33.3% !important;
+}
 </style>
